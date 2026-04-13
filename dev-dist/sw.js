ddd11c7518c898038a6dcd69153e94427d6d7b85
diritditdit07 <aditpermanaputra36@gmail.com>
2026-04-13 11:30:29 +0700

    feat: add Kasir Warung brand logo icon (SVG) + PWA meta tags

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d70286d7'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.64em8gih17"
+    "revision": "0.g07g8rh11co"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
